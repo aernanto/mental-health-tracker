a440aab8a64dff1b594646c3fdd95a0d535b4946
aernanto <exhamusgoldfeder@gmail.com>
2024-11-13 14:51:04 +0700

    Buat tutorial 8

--- mental_health_tracker/urls.py
+++ mental_health_tracker/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
-from main.views import register
+from main.views import registerzzz
 from main.views import logout_user, add_mood_entry_ajax
 
 urlpatterns = [
@@ -26,4 +26,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('logout/', logout_user, name='logout'),
     path('create-mood-entry-ajax', add_mood_entry_ajax, name='add_mood_entry_ajax'),
+    path('auth/', include('authentication.urls')),
 ]
